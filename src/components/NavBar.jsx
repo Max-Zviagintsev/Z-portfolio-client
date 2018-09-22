c97d9791744e701e7e6c8a7ecec16e08c9a879d0
Maxim Zviagintsev <maxim.zviagintsev@gmail.com>
2018-09-22 22:43:11 +0300

    yarn lock

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,61 +1,87 @@
 import React, {Component} from 'react';
-import { Menu, Icon, Button } from 'antd';
+import {Keyframes, animated, config} from 'react-spring';
+import delay from 'delay';
+import { Icon} from 'antd';
 
-const SubMenu = Menu.SubMenu;
+const fast = {...config.stiff, restSpeedThreshold: 1, restDisplacementThreshold: 0.01}
+
+// Creates a spring with predefined animation slots
+const Sidebar = Keyframes.Spring({
+    // Slots can take arrays/chains,
+    peek: [
+        {delay: 500, from: {x: -100}, to: {x: 0}, config: fast},
+        {delay: 800, to: {x: -100}, config: config.slow}
+    ],
+    // single items,
+    open: {to: {x: 0}, config: config.default},
+    // or async functions with side-effects
+    close: async call => {
+        await delay(400)
+        await call({to: {x: -100}, config: config.gentle})
+    }
+});
+
+// Creates a keyframed trail
+const Content = Keyframes.Trail({
+    peek: [{delay: 600, from: {x: -100, opacity: 0}, to: {x: 0, opacity: 1}}, {to: {x: -100, opacity: 0}}],
+    open: {delay: 100, to: {x: 0, opacity: 1}},
+    close: {to: {x: -100, opacity: 0}}
+});
+
+const items = [
+    <h2>Link 1</h2>,
+    <h2>Link 2</h2>,
+    <h2>Link 3</h2>,
+    <h2>Link 1</h2>,
+    <h2>Link 4</h2>
+];
 
 class NavBar extends Component {
-    state = {
-        collapsed: false,
+    constructor() {
+        super();
+        this.state = {open: false};
     }
 
-    toggleCollapsed = () => {
-        this.setState({
-            collapsed: !this.state.collapsed,
-        });
-    }
 
     render() {
+        const {open} = this.state;
+
+        const toggle = () => {
+            !open ?
+                this.setState({
+                    open: true
+                }) :
+                this.setState({
+                    open: false
+                })
+        };
+
+        const state = !open ? 'peek' : open ? 'open' : 'close';
+        const icon = open ? 'fold' : 'unfold';
         return (
-            <div style={{ width: 256 }}>
-                <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-                    <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
-                </Button>
-                <Menu
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
-                    mode="inline"
-                    theme="dark"
-                    inlineCollapsed={this.state.collapsed}
-                >
-                    <Menu.Item key="1">
-                        <Icon type="pie-chart" />
-                        <span>Option 1</span>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Icon type="desktop" />
-                        <span>Option 2</span>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                        <Icon type="inbox" />
-                        <span>Option 3</span>
-                    </Menu.Item>
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                        <Menu.Item key="5">Option 5</Menu.Item>
-                        <Menu.Item key="6">Option 6</Menu.Item>
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-                        <Menu.Item key="9">Option 9</Menu.Item>
-                        <Menu.Item key="10">Option 10</Menu.Item>
-                        <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="11">Option 11</Menu.Item>
-                            <Menu.Item key="12">Option 12</Menu.Item>
-                        </SubMenu>
-                    </SubMenu>
-                </Menu>
+            <div>
+                <Icon type={`menu-${icon}`} className="toggle" onClick={toggle}/>
+                <Sidebar native state={state}>
+                    {({x}) => (
+                        <animated.div className="sidebar"
+                                      style={{transform: x.interpolate(x => `translate3d(${x}%,0,0)`)}}>
+                            <Content native keys={items.map((_, i) => i)} config={{tension: 200, friction: 20}}
+                                     state={state}>
+                                {items.map((item, i) => ({x, ...props}) => (
+                                    <animated.div
+                                        style={{
+                                            transform: x.interpolate(x => `translate3d(${x}%,0,0)`),
+                                            ...props
+                                        }}>
+                                        <div className={i === 0 ? 'middle' : ''}>{item}</div>
+                                    </animated.div>
+                                ))}
+                            </Content>
+                        </animated.div>
+                    )}
+                </Sidebar>
             </div>
-        );
+        )
     }
 }
 
