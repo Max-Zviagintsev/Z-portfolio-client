224f19153502eb8d1c8481339fa213ad908d0706
Maxim Zviagintsev <maxim.zviagintsev@gmail.com>
2018-11-13 22:23:51 +0200

    before gallery

--- src/components/PortfolioComponent/CurrentProject.jsx
+++ src/components/PortfolioComponent/CurrentProject.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import {Row, Col} from 'antd';
 
 const CurrentProject = (props) => {
-const usedTech = props.projectData.node.field_used_tech.map(item => <div>{item}</div>);
+    const usedTech = props.projectData.node.field_used_tech.map(item => <div>{item}</div>);
 
     return (
-    <div>
-    <Row>
-    <Col span={16} dangerouslySetInnerHTML={{ __html: props.projectData.node.field_project_description.value }}/>
-    <Col span={8}> {usedTech} </Col>
-</Row>
-    <div>Gallery</div>
-    {console.log(props)}
-</div>
-);
+        <div>
+            <Row>
+                <Col span={16}
+                     dangerouslySetInnerHTML={{__html: props.projectData.node.field_project_description.value}}/>
+                <Col span={8}> {usedTech} </Col>
+            </Row>
+            <div>Gallery</div>
+            {console.log(props)}
+        </div>
+    );
 };
 
 export default CurrentProject;
